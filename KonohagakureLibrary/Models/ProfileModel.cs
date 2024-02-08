540d5653a548013536cb333c2b4e986b08a9568f
Ricardo Montoya <montoya.ar94@gmail.com>
2024-02-08 12:03:30 -0500

    Profile class to represent profile data from PostgreSQL database

---
+++ KonohagakureLibrary/Models/ProfileModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonohagakureLibrary.Models
+{
+	public class ProfileModel
+	{
+		public ulong MemberId { get; set; }
+		public string Username { get; set; }
+		public ulong ServerId { get; set; }
+		public string ServerName { get; set; }
+		public string AvatarURL { get; set; }
+		public string ProfileImage { get; set; }
+		public string InGameName { get; set; }
+		public int Level { get; set; } = 0;
+		public string Clan { get; set; } = "—";
+		public string Organization { get; set; } = "—";
+		public string OrganizationRank { get; set; } = "—";
+		public int Raids { get; set; } = 0;
+		public int Fame { get; set; } = 0;
+		public int ProctoredMissions { get; set; } = 0;
+		public string Masteries { get; set; } = "—";
+		public string Alts { get; set; } = "—";
+	}
+}
