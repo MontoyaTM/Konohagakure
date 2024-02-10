411c7752be56e22023430d58e2fc81cf5bfa70d3
Ricardo Montoya <montoya.ar94@gmail.com>
2024-02-10 16:19:07 -0500

    Added embed message to describe button functionality

--- Konohagakure/VillagerApplication/PrefixCommandVillagerApplication.cs
+++ Konohagakure/VillagerApplication/PrefixCommandVillagerApplication.cs
@@ -16,11 +16,12 @@ namespace Konohagakure.VillagerApplication
 {
 	public class PrefixCommandVillagerApplication : BaseCommandModule
 	{
-		[Command("villager_application")]
+        [Command("villager_application")]
 		[RequireRoles(RoleCheckMode.Any, "Administrator")]
+		[Description("Displays an embed message for villager applications.")]
 		public async Task VillagerApplication(CommandContext ctx)
 		{
-			ButtonCommandsExtension buttonCommand = ctx.Client.GetButtonCommands();
+			ButtonCommandsExtension buttonCommand = ctx.Client.GetButtonCommands();			
 
 			var embedApplication = new DiscordMessageBuilder()
 				.AddEmbed(new DiscordEmbedBuilder()
@@ -40,7 +41,31 @@ namespace Konohagakure.VillagerApplication
 
 			await ctx.Message.DeleteAsync();
 			await ctx.Channel.SendMessageAsync(embedApplication);
+		}
 
+		[Command("application_information")]
+		[RequireRoles(RoleCheckMode.Any, "Administrator")]
+		[Description("Displays an Embed used as a guide to accepting and denying applications.")]
+		public async Task ApplicationInfo(CommandContext ctx)
+		{
+			ButtonCommandsExtension buttonCommand = ctx.Client.GetButtonCommands();
+
+			var embedApplication = new DiscordMessageBuilder()
+					.AddEmbed(new DiscordEmbedBuilder()
+						.WithColor(DiscordColor.SpringGreen)
+						.WithTitle($"Button Information")
+						.WithThumbnail(Images.LeafSymbol_URL)
+						.AddField("Accept Applicant:", "Pressing this button will grant the user Genin role and their application will be sent to the application-accepted channel for storage. ")
+						.AddField("Deny Applicant:", "Pressing this button will display an embed message asking you to type the reason for denying the applicant. " +
+													 "Your response will then be sent to the user and their application will be sent to the application-denied channel.")
+						.WithFooter("The buttons are for display purposes, they do not have functionality.")
+					).AddComponents(new DiscordComponent[]
+					{
+						new DiscordButtonComponent(ButtonStyle.Primary, buttonCommand.BuildButtonId("displayOnly1"), "Accept Applicant"),
+						new DiscordButtonComponent(ButtonStyle.Secondary, buttonCommand.BuildButtonId("displayOnly2"), "Deny Applicant")
+					});
+			await ctx.Message.DeleteAsync();
+			await ctx.Channel.SendMessageAsync(embedApplication);
 		}
 	}
 }
