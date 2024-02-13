383c1b320df7d49e8c121b6ce22ad1c530ac11e1
Ricardo Montoya <montoya.ar94@gmail.com>
2024-02-12 19:30:43 -0500

    Added ILogger Added IDatabaseHokageData for Hokage dashboard database access Added SlashCommand cooldown Added Select Menu interaction handler

--- Konohagakure/DiscordBot.cs
+++ Konohagakure/DiscordBot.cs
@@ -11,7 +11,10 @@ using DSharpPlus.ModalCommands;
 using DSharpPlus.ModalCommands.EventArgs;
 using DSharpPlus.ModalCommands.Extensions;
 using DSharpPlus.SlashCommands;
+using DSharpPlus.SlashCommands.Attributes;
 using DSharpPlus.SlashCommands.EventArgs;
+using Konohagakure.Dashboards.Hokage;
+using Konohagakure.Profile;
 using Konohagakure.VillagerApplication;
 using KonohagakureLibrary.Data;
 using KonohagakureLibrary.DBUtil;
@@ -23,16 +26,18 @@ namespace Konohagakure
 	{
 		private readonly ILogger<DiscordBot> _logger;
 		private readonly IConfiguration _config;
+		private readonly IDatabaseProfileData _db;
 
 		private DiscordClient Client;
 		private CommandsNextExtension Commands;
 		private ButtonCommandsExtension ButtonCommands;
 		private ModalCommandsExtension ModalCommands;
 
-		public DiscordBot(ILogger<DiscordBot> logger, IConfiguration config)
+		public DiscordBot(ILogger<DiscordBot> logger, IConfiguration config, IDatabaseProfileData _db)
 		{
 			_logger = logger;
 			_config = config;
+			this._db = _db;
 		}
 
 		public override async Task StartAsync(CancellationToken cancellationToken)
@@ -56,9 +61,9 @@ namespace Konohagakure
 			// 3. Register Services for DSharpPlus's DependencyInjection
 			var botServices = new ServiceCollection()
 				.AddSingleton<IConfiguration>(_config)
+				.AddTransient<IDatabaseHokageData,  PostgreSQLHokageData>()
 				.AddTransient<IDatabaseProfileData, PostgreSQLProfileData>()
 				.AddTransient<IPostgreSQLDataAccess, PostgreSQLDataAccess>()
-				
 					.BuildServiceProvider();
 
 			// 4. Default timeout for Commands using Interactivity
@@ -82,6 +87,7 @@ namespace Konohagakure
 			{
 				ArgumentSeparator = "-",
 				Prefix = botPrefix,
+				Services = botServices
 				
 			};
 			ButtonCommands = Client.UseButtonCommands(buttonCommandsConfig);
@@ -94,7 +100,12 @@ namespace Konohagakure
 			};
 			ModalCommands = Client.UseModalCommands(modalCommandsConfig);
 
-			var slashCommands = Client.UseSlashCommands();
+			var slashCommandsConfig = new SlashCommandsConfiguration()
+			{
+				Services = botServices
+			};
+
+			var slashCommands = Client.UseSlashCommands(slashCommandsConfig);
 
 			// 5. Creating Task Handlers
 			Client.Ready += OnClient_Ready;
@@ -122,16 +133,49 @@ namespace Konohagakure
 
 			#endregion
 
+			#region Hokage Dashboard
+
+			Commands.RegisterCommands<PrefixCommandHokageDashboard>();
+			ButtonCommands.RegisterButtons<ButtonCommandHokageDashboard>();
+
+			#endregion
+
+			#region Profile
+
+			slashCommands.RegisterCommands<SlashCommandProfile>();
+			Commands.RegisterCommands<PrefixCommandProfile>();
+
+			#endregion
+
 			// 7. Connecting...
 			await Client.ConnectAsync();
 		}
 
-		private Task OnSlashCommands_SlashCommandErrored(SlashCommandsExtension sender, SlashCommandErrorEventArgs args)
+		private async Task OnSlashCommands_SlashCommandErrored(SlashCommandsExtension sender, SlashCommandErrorEventArgs args)
 		{
 			_logger.LogError(args.Exception,
 				$"{ args.Context.Member } has used slash command { args.Context.CommandName } which threw an Exception: {args.Exception.Message}");
 
-			return Task.CompletedTask;
+			if (args.Exception is SlashExecutionChecksFailedException exception)
+			{
+				string timeLeft = string.Empty;
+				foreach (var check in exception.FailedChecks)
+				{
+					var cooldown = (SlashCooldownAttribute)check;
+					timeLeft = cooldown.GetRemainingCooldown(args.Context).ToString(@"hh\:mm\:ss");
+				}
+
+				var coolDownMessage = new DiscordEmbedBuilder()
+				{
+					Color = DiscordColor.Red,
+					Title = "Please wait for the cooldown to end.",
+					Description = $"Time: {timeLeft}"
+				};
+
+				await args.Context.CreateResponseAsync(coolDownMessage);
+			}
+
+			
 		}
 
 		private Task OnModalCommands_ModalCommandErrored(ModalCommandsExtension sender, ModalCommandErrorEventArgs args)
@@ -183,9 +227,64 @@ namespace Konohagakure
 			return Task.CompletedTask;
 		}
 
-		private Task OnClient_ComponentInteractionCreated(DiscordClient sender, ComponentInteractionCreateEventArgs args)
+		private async Task OnClient_ComponentInteractionCreated(DiscordClient sender, ComponentInteractionCreateEventArgs args)
 		{
-			return Task.CompletedTask;
+			if (args.Interaction.Data.ComponentType == ComponentType.StringSelect)
+			{
+				switch (args.Interaction.Data.CustomId)
+				{
+					case "dpdwn_ClanEmoji":
+						await args.Interaction.DeferAsync(true);
+						string clanSelected = args.Interaction.Data.Values[0];
+
+						var userExists_Clan = await _db.UserExistsAsync(args.Interaction.User.Id);
+
+						if (!userExists_Clan)
+						{
+							await args.Interaction.CreateFollowupMessageAsync(new DiscordFollowupMessageBuilder().WithContent("You do not have a profile in the database!"));
+							break;
+						}
+
+						var isClanUpdated = await _db.UpdateClanAsync(args.User.Id, clanSelected);
+
+						if (isClanUpdated)
+						{
+							await args.Interaction.CreateFollowupMessageAsync(new DiscordFollowupMessageBuilder().WithContent($"Successfully updated your clan!"));
+						}
+						else
+						{
+							await args.Interaction.CreateFollowupMessageAsync(new DiscordFollowupMessageBuilder().WithContent($"Failed to update your clan!"));
+						}
+						break;
+
+
+					case "dpdwn_MasteryEmoji":
+						await args.Interaction.DeferAsync(true);
+						var masterySelected = args.Interaction.Data.Values.ToArray();
+
+						var userExists_Mastery = await _db.UserExistsAsync(args.Interaction.User.Id);
+
+						if (!userExists_Mastery)
+						{
+							await args.Interaction.CreateFollowupMessageAsync(new DiscordFollowupMessageBuilder().WithContent("You do not have a profile in the database!"));
+							break;
+						}
+
+						var isMasteryUpdated = await _db.UpdateMasteriesAsync(args.User.Id, masterySelected);
+
+						if (isMasteryUpdated)
+						{
+							await args.Interaction.CreateFollowupMessageAsync(new DiscordFollowupMessageBuilder().WithContent($"Successfully updated your masteries!"));
+						}
+						else
+						{
+							await args.Interaction.CreateFollowupMessageAsync(new DiscordFollowupMessageBuilder().WithContent($"Failed to update your masteries!"));
+						}
+						break;
+				}
+
+
+			}
 		}
 
 		protected override Task ExecuteAsync(CancellationToken stoppingToken) => Task.CompletedTask;
