c53b7de2c21234a596df63c93515e0dc3bfae627
Ricardo Montoya <montoya.ar94@gmail.com>
2024-02-19 16:05:36 -0500

    Created a request model for RP Requests to mirror database data

---
+++ KonohagakureLibrary/Models/RequestModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonohagakureLibrary.Models
+{
+	public class RequestModel
+	{
+		public ulong RequestId { get; set; }
+		public ulong MemberId { get; set; }
+		public string Username { get; set; }
+		public ulong ServerId { get; set; }
+		public string ServerName { get; set; }
+		public string InGameName { get; set; }
+		public string Mission {  get; set; }
+		public string Attendees { get; set; }
+		public string Timezone { get; set; }
+	}
+}
